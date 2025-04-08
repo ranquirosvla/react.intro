29682234a7e349a0380ed0e93b1f6726807b01ed
ranquirosvla <rsqrodriguez@vlacademy.com>
2025-04-07 20:23:40 -0600

    Creamos un menu parecido a github

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,25 +13,87 @@ function HelloWorld() {
 }
 
 function App() {
+
+  const logotipo = logo;
+
+  const mostrarImagen = true;
+
+  const number = 3;
+  const mostrarNumber = number > 3; //mayor de 5 es true
+  // const mostrarNumber = number > 5 ? true : false; //mayor de 5 es true
+
+  const menuGithub = [ 
+    { id: 1, nombre: 'Code', url: 'https://github.com/code' },
+    { id: 2, nombre: 'Issues', url: 'https://github.com/issues' },
+    { id: 3, nombre: 'Pull-request', url: 'https://github.com/pull-request' },
+  ] ;
+
+  const menu = menuGithub.map(item => {
+    return (
+      <div>
+      {item.nombre}
+      </div>
+    )
+  }
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+
+        <img src={logotipo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       </header>
       <HelloWorld />
+      <div>
+      {/* react */}
+      {mostrarImagen? <img src={logotipo} className="App-logo" alt="logo" /> : 'No hay imagen para mostrar'}
+
+      {/* vue */}
+      {/* <img v-if="mostrarImagen" src={logotipo} className="App-logo" alt="logo" />
+      <span v-else>No hay imagen</span> */}
+
+      <div> 
+        {mostrarNumber ?  'El numero es menor a 5' : 'El numero es mayor a 5'}
+      </div> 
+
+      <div> 
+        {number > 5 ?  'El numero es mayor a 5' : 'El numero es menor a 5'}
+      </div> 
+
+      <div> 
+      {mostrarNumber ? (
+        'El numero es menor a 5' 
+        )  : (
+        'El numero es mayor a 5' 
+        )} 
+      </div>
+
+      <div> 
+      {mostrarNumber && 'El numero es mayor a 5' }
+      </div>
+
+    </div>
+
+      <div>
+        {menu}
+      </div>
+    
     </div>
   );
 }
+
 
 export default App;
